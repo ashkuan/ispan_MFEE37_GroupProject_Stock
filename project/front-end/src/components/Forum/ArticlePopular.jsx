5b36ab8e9e8ef7e024c06c1e0c523a291d3c4c0e
ashkuan <everyfun147@gmail.com>
2023-07-05 15:48:24 +0800

    Ash forum right board

--- project/front-end/src/components/Forum/ArticlePopular.jsx
+++ project/front-end/src/components/Forum/ArticlePopular.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Billboard from "./Billboard";
+import "../../styles/forum_main_right.css";
 
 function ArticlePopular() {
-  return <div>熱門</div>;
+  return (
+    <div className="articlePopular mt-4">
+      <Billboard />
+    </div>
+  );
 }
 
 export default ArticlePopular;
