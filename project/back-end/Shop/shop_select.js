f8305df892a9ea88a0814bb34af7cc50696e0ca7
Kristy Yu <tzuchi.yu1@gmail.com>
2023-07-05 17:07:28 +0800

    edit css

--- project/back-end/Shop/shop_select.js
+++ project/back-end/Shop/shop_select.js
@@ -1,14 +1,12 @@
-// 不可更改 !!!!!!!!!!!!!
-
 import express from "express";
 import cors from "cors";
 import db from "../DB/DBconfig.js";
 var app = express();
-
 app.use(cors());
+app.use(express.json()); // 將接收到的JSON格式的資料轉換為JS物件
 
 app.get("/", function (req, res) {
-  res.send("連接成功");
+  res.send("select連接成功");
 });
 
 app.get("/shop", function (req, res) {
@@ -21,6 +19,108 @@ app.get("/shop", function (req, res) {
   });
 });
 
+app.post("/shop/cart/add", function (req, res) {
+  const cartItems = req.body.data;
+  // 過濾為0的pid
+  const filteredItems = Object.entries(cartItems).filter(
+    ([pid, paccount]) => paccount !== 0
+  );
+  const VALUES = filteredItems.map(([pid, paccount]) => [pid, paccount]);
+  console.log(VALUES);
+
+  db.query(
+    "INSERT INTO Cart (pid, paccount) VALUES ? ON DUPLICATE KEY UPDATE paccount = VALUES(paccount)",
+    [VALUES],
+    function (err, data) {
+      if (err) {
+        console.error("資料庫新增失敗:", err);
+      } else {
+        console.log("資料庫新增成功");
+      }
+    }
+  );
+});
+
+app.delete("/shop/cart", function (req, res) {
+  const cartItems = req.body.data;
+  console.log(cartItems);
+  // 過濾為0的pid
+  const filteredItems = Object.entries(cartItems).filter(
+    ([pid, paccount]) => paccount !== 0
+  );
+  const removeValues = filteredItems.map(([pid, paccount]) => [
+    pid,
+    paccount - 1,
+  ]);
+  const deletePids = Object.entries(cartItems)
+    .filter(([pid, paccount]) => paccount === 0)
+    .map(([pid, paccount]) => pid);
+
+  // db.query(
+  //   `DELETE FROM Cart WHERE pid IN (${deletePids.map(() => "?").join(",")})`,
+  //   [deletePids],
+  //   function (err, data) {
+  //     if (err) {
+  //       console.error("資料庫刪除失敗:", err);
+  //     } else {
+  //       console.log("資料庫刪除成功");
+  //       // 執行更新操作
+  //       db.query(
+  //         "INSERT INTO Cart (pid, paccount) VALUES ? ON DUPLICATE KEY UPDATE paccount = VALUES(paccount)",
+  //         [removeValues],
+  //         function (err, data) {
+  //           if (err) {
+  //             console.error("刪除失敗:", err);
+  //           } else {
+  //             console.log("刪除成功");
+  //           }
+  //         }
+  //       );
+  //     }
+  //   }
+  // );
+});
+
+// app.post("/shop", function (req, res) {
+//   const items = req.body.data;
+//   console.log("這是items");
+//   console.log(items);
+//   // console.log(items);
+//   // const pids = items.filter(([pid, paccount]) => paccount == 0); // 獲取商品數量為0的所有pid
+//   // console.log("這是商品數為0的pid們");
+//   // console.log(pids);
+
+//   // if (pids.length > 0) {
+//   //   db.query(
+//   //     // 要變成(?,?,?)；IN檢查值是否在資料表中
+//   //     `DELETE FROM Cart WHERE pid IN (${pids.map(() => "?").join(",")})`,
+//   //     pids,
+//   //     function (err, data) {
+//   //       if (err) {
+//   //         console.error("失敗，無法刪除商品數為0的pid:", err);
+//   //       } else {
+//   //         console.log("已更新商品數為0的pid");
+//   //       }
+//   //     }
+//   //   );
+//   // }
+
+//   const updateValues = items.filter(([pid, paccount]) => [pid, paccount]);
+//   if (updateValues.length > 0) {
+//     db.query(
+//       "INSERT INTO Cart (pid, paccount) VALUES ? ON DUPLICATE KEY UPDATE paccount = VALUES(paccount)",
+//       [updateValues],
+//       function (err, data) {
+//         if (err) {
+//           console.error("購物車儲存失敗:", err);
+//         } else {
+//           console.log("購物車儲存成功");
+//         }
+//       }
+//     );
+//   }
+// });
+
 app.listen(3000, () => {
-  console.log("port 3000連接完成" + new Date().toLocaleTimeString());
+  console.log("Shop 的 port 3000 連接完成 " + new Date().toLocaleTimeString());
 });
--- project/front-end/context/ShopContext.jsx
+++ project/front-end/context/ShopContext.jsx
@@ -3,6 +3,7 @@ import React, { createContext, useEffect, useState } from "react";
 
 export const ShopContext = createContext(null);
 
+// 設定商品和商品數預設值
 const getDefaultCart = () => {
   let cart = {};
   for (let i = 0; i < 19; i++) {
@@ -11,11 +12,39 @@ const getDefaultCart = () => {
   return cart;
 };
 
+// 傳入後端
+const addItemsToDB = async (cartItems) => {
+  console.log("執行addItemsToDB()");
+  console.log(cartItems);
+  try {
+    // 執行fetch請求將cartItems傳送到後端API進行資料庫儲存
+    const res = await axios.post("http://localhost:3000/shop/cart/add", {
+      data: cartItems,
+    });
+  } catch (error) {
+    console.error("傳入後端失敗:", error);
+  }
+};
+const removeItemsFromDB = async (cartItems) => {
+  console.log("執行removeItemsFromDB()");
+  console.log(cartItems);
+  try {
+    // 執行fetch請求將cartItems傳送到後端API進行資料庫儲存
+    const res = await axios.delete("http://localhost:3000/shop/cart", {
+      data: cartItems,
+    });
+  } catch (error) {
+    console.error("傳入後端失敗:", error);
+  }
+};
+
 export const ShopContextProvider = (props) => {
   const [products, setProducts] = useState([]);
   const [totalAmount, setTotalAmount] = useState(0);
-  const [cartItems, setCartItems] = useState({});
+  const [cartItems, setCartItems] = useState(getDefaultCart());
+  const [DBItems, setDBItems] = useState([]);
 
+  // select from
   useEffect(() => {
     const fetchShop = async () => {
       try {
@@ -23,7 +52,8 @@ export const ShopContextProvider = (props) => {
         const res = await axios.get(`http://localhost:3000/shop`);
         setProducts(res.data); // 把後端所有商品資料放入state;
         setTotalAmount(res.data.length); // 商品總數
-        setCartItems(getDefaultCart()); // 一開始預設的商品和商品數
+        // setCartItems(getDefaultCart()); // 一開始預設的商品和商品數
+        console.log(cartItems);
       } catch (err) {
         console.log(err);
       }
@@ -31,17 +61,49 @@ export const ShopContextProvider = (props) => {
     fetchShop();
   }, []);
 
+  // insert into；cartItems更新時要執行以下，但無法成功執行，因此改為設定定時更新
+  // useEffect(() => {
+  // console.log(cartItems);
+  // cartItems是物件，所以要用entries和forEach抓出key和value
+  // Object.entries(cartItems).forEach(([pid, paccount]) => {
+  //   const item = [pid, paccount];
+  //   DBItems.push(item); // [key,value]
+  //   setDBItems(DBItems);
+  // });
+
+  // 設定定時更新
+  // const saveInterval = setInterval(() => {
+  // saveCartItemsToDB(DBItems);
+  // }, 1000);
+  //  定時功能在元件渲染時執行，等元件卸載時就清除
+  // return () => {
+  // clearInterval(saveInterval);
+  // };
+  // }, [cartItems]);
+
   const addToCart = (pid) => {
+    console.log("點擊新增按鈕");
     // 找到cartItems目前的值(預設為0) => 1. 顯示出所有cartItems  2. [將點擊的id]:原本的[商品]數量+1
     setCartItems((prev) => ({ ...prev, [pid]: prev[pid] + 1 }));
+    console.log("新增cartItems");
+    const updatedCartItems = { ...cartItems, [pid]: cartItems[pid] + 1 };
+    addItemsToDB(updatedCartItems);
   };
 
   const removeFromCart = (pid) => {
+    console.log("點擊刪除按鈕");
     setCartItems((prev) => ({ ...prev, [pid]: prev[pid] - 1 }));
+    console.log("刪除cartItems");
+    const updatedCartItems = { ...cartItems, [pid]: cartItems[pid] - 1 };
+    removeItemsFromDB(updatedCartItems);
   };
 
   const updateCartItemAmount = (newAmount, pid) => {
     setCartItems((prev) => ({ ...prev, [pid]: newAmount }));
+  };
+
+  const trashCan = (pid) => {
+    setCartItems((prev) => ({ ...prev, [pid]: 0 }));
   };
 
   const contextValue = {
@@ -51,6 +113,7 @@ export const ShopContextProvider = (props) => {
     addToCart,
     removeFromCart,
     updateCartItemAmount,
+    trashCan,
   };
 
   // console.log(cartItems);
