5861285990a5b9b53391cead847e30608c8dec2b
Kristy Yu <tzuchi.yu1@gmail.com>
2023-07-17 02:13:22 +0800

    edit Cart

--- project/back-end/Shop/ECpay.js
+++ project/back-end/Shop/ECpay.js
@@ -1,6 +1,7 @@
 import express from "express";
 import axios from "axios";
 import db from "../DB/DBconfig.js";
+import crypto from "crypto";
 
 const app = express();
 app.use(express.json());
@@ -15,58 +16,33 @@ function formatDateTime(date) {
   const second = String(date.getSeconds()).padStart(2, "0");
   return `${year}/${month}/${day} ${hour}:${minute}:${second}`;
 }
-const oid = Math.floor(Math.random() * 1000000000).toString();
-console.log("這是訂單編號：" + oid);
 
-const merchantID = "3002607"; // 特店編號
+const MerchantID = "3002607"; // 特店編號
 const hashKey = "pwFHCqoQZGmho4w6";
 const hashIV = "EkRm7iFT261dpevs";
-const apiUrl = "https://payment.greenworld.com.tw/Cashier/AioCheckOut/V5";
+const apiUrl = "https://payment-stage.ecpay.com.tw/Cashier/AioCheckOut/V5";
 
-const merchantTradeNo = oid; // 特店訂單編號
-const merchantTradeDate = formatDateTime(new Date()); // 特店交易時間
-console.log("這是訂單成立時間：" + merchantTradeDate);
-const paymentType = "aio"; // 交易類型
-const tradeDesc = "Payment for Order"; // 交易描述
-const itemName = "Product Name"; // 商品名稱
-const returnURL = "https://example.com/return"; // 付款完成通知回傳網址
-const choosePayment = "ALL"; // 選擇預設付款方式
-const checkMacValue = "CHECK_MAC_VALUE"; // 檢查碼
-const encryptType = 1; // CheckMacValue加密類型
-// const storeID = "STORE_ID"; // 特店旗下店舖代號
-const clientBackURL = "https://example.com/back"; // Client端返回特店的按鈕連結
-const itemURL = "https://example.com/product"; // 商品銷售網址
-const remark = "Additional remarks"; // 備註欄位
-const chooseSubPayment = ""; // 付款子項目
-const orderResultURL = "https://example.com/result"; // Client端回傳付款結果網址
-const needExtraPaidInfo = "N"; // 是否需要額外的付款資訊
-
-// const params = {
-//   MerchantID: merchantID,
-//   MerchantTradeNo: merchantTradeNo,
-//   MerchantTradeDate: merchantTradeDate,
-//   PaymentType: paymentType,
-//   TotalAmount: totalAmount,
-//   TradeDesc: tradeDesc,
-//   ItemName: itemName,
-//   ReturnURL: returnURL,
-//   ChoosePayment: choosePayment,
-//   CheckMacValue: checkMacValue,
-//   EncryptType: encryptType,
-//   StoreID: storeID,
-//   ClientBackURL: clientBackURL,
-//   ItemURL: itemURL,
-//   Remark: remark,
-//   ChooseSubPayment: chooseSubPayment,
-//   OrderResultURL: orderResultURL,
-//   NeedExtraPaidInfo: needExtraPaidInfo,
-// };
+// const PaymentType = "aio"; // 交易類型
+const ReturnURL = "http://localhost:5173/shop/checkout/done";
+console.log(ReturnURL); // 付款完成通知回傳網址
+// const ChoosePayment = "ALL"; // 選擇預設付款方式
+// const encryptType = 1; // CheckMacValue加密類型
+// const clientBackURL = "https://example.com/back"; // Client端返回特店的按鈕連結
+// const itemURL = "https://example.com/product"; // 商品銷售網址
 
 app.post("/sendOrder", async (req, res) => {
   try {
+    const MerchantTradeDate = formatDateTime(new Date()); // 特店交易時間
+    console.log("這是訂單成立時間：");
+    console.log(MerchantTradeDate);
+    const oid = Math.floor(Math.random() * 1000000000).toString();
+    const MerchantTradeNo = oid; // 特店訂單編號
+    console.log("這是訂單編號：");
+    console.log(MerchantTradeNo);
+
     const {
-      email,
-      LogisticsType,
+      //   email,
+      //   LogisticsType,
       message,
       pid,
       oitemName,
@@ -74,17 +50,37 @@ app.post("/sendOrder", async (req, res) => {
       totalAmount,
     } = req.body;
 
-    const TotalAmount = totalAmount.slice(4, totalAmount.length);
-    // console.log(totalAmount2);
+    const TradeDesc = message; // 交易描述
+    console.log("這是交易描述：");
+    console.log(TradeDesc);
+    const TotalAmount = totalAmount.slice(4, totalAmount.length); // 訂單總金額
+    console.log("這是訂單總金額：");
+    console.log(TotalAmount);
+    const ItemName = oitemName;
+    let MyItemName = ""; // 商品名稱
+    console.log("這是ItemName：");
+    oitemName.map((item) => {
+      MyItemName += item + "<br></br>";
+    });
+
+    // 生成 CheckMacValue
+    const input = `HashKey=${hashKey}&ChoosePayment=ALL&EncryptType=1&ItemName=Stock Shop Item x1&MerchantID=${MerchantID}&MerchantTradeDate=${MerchantTradeDate}&MerchantTradeNo=${MerchantTradeNo}&PaymentType=aio&ReturnURL=${ReturnURL}&TotalAmount=${TotalAmount}&TradeDesc=${TradeDesc}&HashIV=${hashIV}`;
+    console.log("這是input：");
+    console.log(input);
+    const hash = crypto.createHash("sha256");
+    hash.update(input);
+    const CheckMacValue = hash.digest("hex").toUpperCase();
+    console.log("這是CheckMacValue：");
+    console.log(CheckMacValue);
 
     const orderData = [
-      oid,
+      MerchantTradeNo,
       "u01",
       pid.toString(),
-      merchantTradeDate,
+      MerchantTradeDate,
       TotalAmount,
-      message,
-      oitemName.toString(),
+      TradeDesc,
+      ItemName.toString(),
       coupon,
     ];
 
@@ -96,64 +92,39 @@ app.post("/sendOrder", async (req, res) => {
         console.log("訂單儲存失敗");
       } else {
         console.log("訂單儲存成功");
-        res.send("訂單已送出");
+        // res.send("訂單已送出");
       }
     });
+
+    const params = {
+      MerchantID: MerchantID,
+      MerchantTradeNo: MerchantTradeNo, // 生成唯一的特店訂單編號
+      MerchantTradeDate: MerchantTradeDate, // 特店交易時間
+      PaymentType: "aio",
+      TotalAmount: TotalAmount,
+      TradeDesc: TradeDesc,
+      ItemName: "Stock Shop Item x1",
+      ReturnURL: ReturnURL,
+      ChoosePayment: "ALL",
+      EncryptType: "1",
+      CheckMacValue,
+    };
+
+    axios
+      .post(apiUrl, params, {
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      })
+      .then((response) => {
+        res.send("成功" + response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   } catch (err) {
     console.log("接收失敗");
     console.log(err);
-    res.send(err);
   }
 });
-
-// app.post("/checkout", async (req, res) => {
-//   try {
-//     const { amount, orderId, redirectUrl } = req.body;
-
-//     // 构建支付参数
-//     const paymentParams = {
-//       MerchantID: merchantID,
-//       MerchantTradeNo: orderId,
-//       MerchantTradeDate: new Date()
-//         .toISOString()
-//         .slice(0, 19)
-//         .replace("T", " "),
-//       PaymentType: "aio",
-//       TotalAmount: amount,
-//       TradeDesc: "Payment for Order",
-//       ItemName: "Order Item",
-//       ReturnURL: redirectUrl,
-//       ChoosePayment: "Credit",
-//       ClientBackURL: redirectUrl,
-//     }; // 生成 CheckMacValue
-//     const checkMacValue = generateCheckMacValue(paymentParams);
-
-//     // 添加 CheckMacValue 到支付参数中
-//     paymentParams.CheckMacValue = checkMacValue;
-
-//     // 发送支付请求
-//     const response = await axios.post(apiUrl, qs.stringify(paymentParams), {
-//       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-//     });
-
-//     // 将綠界支付 API 的响应返回给前端
-//     res.send(response.data);
-//   } catch (error) {
-//     console.error("Payment failed:", error);
-//     res.status(500).send("Payment failed");
-//   }
-// });
-
-// axios
-//   .post(apiUrl, qs.stringify(params), {
-//     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-//   })
-//   .then((response) => {
-//     console.log(response.data);
-//   })
-//   .catch((error) => {
-//     console.error(error);
-//   });
 
 app.listen(5567, () => {
   console.log("Shop 的 port 5567 連接完成 " + new Date().toLocaleTimeString());
