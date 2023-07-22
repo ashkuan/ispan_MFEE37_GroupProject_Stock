b471bf11c643f48593a27908cbbbae5fa03c2bfd
Kristy Yu <tzuchi.yu1@gmail.com>
2023-07-22 23:54:43 +0800

    edit bash

---
+++ start.sh
@@ -0,0 +1,7 @@
+nodemon ./project/back-end/Forum/message.js &
+nodemon ./project/back-end/Forum/post.js &
+nodemon ./project/back-end/member/member.js &
+nodemon ./project/back-end/Rookie/rookie.js &
+nodemon ./project/back-end/Shop/shop.js &
+cd ./project/back-end/Stock
+nodemon stock.js &
