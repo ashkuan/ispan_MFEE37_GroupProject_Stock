5ae81e73367ecc9df07c38bd04f70491e2204f39
Kristy Yu <tzuchi.yu1@gmail.com>
2023-07-23 21:20:38 +0800

    edit shop

---
+++ project/back-end/Shop/sampleData.js
@@ -0,0 +1,21 @@
+const orders = {
+  1: {
+    amount: 1000,
+    currency: "TWD",
+    packages: [
+      {
+        id: "products_1",
+        amount: 1000,
+        products: [
+          {
+            name: "六角棒棒",
+            quantity: 1,
+            price: 1000,
+          },
+        ],
+      },
+    ],
+  },
+};
+
+module.exports = orders;
