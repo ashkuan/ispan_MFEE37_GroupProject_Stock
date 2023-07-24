37b61f6c17795cd95369e408f5c86cce8ded8d45
Kristy Yu <tzuchi.yu1@gmail.com>
2023-07-24 14:59:08 +0800

    edit EnterEmail

--- project/front-end/src/components/member/EnterEmail.jsx
+++ project/front-end/src/components/member/EnterEmail.jsx
@@ -26,7 +26,12 @@ const EnterEmail = () => {
     <>
       {!emailSent ? (
         <div className="mt-10_5rem d-flex align-items-center justify-content-center">
-          <form id="memberLogin" className="card p-4 rounded-4 drop-shadow-20">
+          <form
+            action="http://localhost:3333/sendEmail"
+            method="post"
+            id="memberLogin"
+            className="card p-4 rounded-4 drop-shadow-20"
+          >
             <div className="card-body fw-bold px-5 text-IronGray-Deep">
               <div className="">
                 <label
@@ -42,9 +47,9 @@ const EnterEmail = () => {
                   placeholder="請輸入 Email"
                 />
               </div>
-
               <div className="d-flex flex-column justify-content-around">
                 <button
+                  type="submit"
                   onClick={handlePass}
                   className="btn btn-login py-2 mb-4 mt-4"
                 >
@@ -60,7 +65,51 @@ const EnterEmail = () => {
             </div>
           </form>
         </div>
-      ) : null}
+      ) : (
+        <div
+          className="mt-10_5rem align-items-center justify-content-center"
+          style={{ display: "none" }}
+        >
+          <form
+            action="http://localhost:3333/sendEmail"
+            method="post"
+            id="memberLogin"
+            className="card p-4 rounded-4 drop-shadow-20"
+          >
+            <div className="card-body fw-bold px-5 text-IronGray-Deep">
+              <div className="">
+                <label
+                  htmlFor="email"
+                  className="d-flex justify-content-center m-auto py-3 fs-3"
+                >
+                  忘記密碼
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  className="member-inp border-1 rounded-2"
+                  placeholder="請輸入 Email"
+                />
+              </div>
+              <div className="d-flex flex-column justify-content-around">
+                <button
+                  type="submit"
+                  onClick={handlePass}
+                  className="btn btn-login py-2 mb-4 mt-4"
+                >
+                  發送驗證信
+                </button>
+                <button
+                  onClick={handleClose}
+                  className="btn btn-login py-2 mb-4"
+                >
+                  取消
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      )}
     </>
   );
 };
