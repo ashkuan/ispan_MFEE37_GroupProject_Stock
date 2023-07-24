4bdb592191c139bd817c909eb80f4718126937bd
pcsara0923 <pcsara0923@gmail.com>
2023-07-24 14:47:37 +0800

    update enteremail

--- project/front-end/src/components/member/EnterEmail.jsx
+++ project/front-end/src/components/member/EnterEmail.jsx
@@ -16,48 +16,51 @@ const EnterEmail = () => {
   const navigate = useNavigate();
   const [errors, setErrors] = useState({});
 
-const handleClose =()=>{
-  navigate('/loginpage')
-}
+  const handleClose = () => {
+    navigate("/loginpage");
+  };
+  const handlePass = () => {
+    setEmailSent(true);
+  };
   return (
     <>
-      <div className="mt-10_5rem d-flex align-items-center justify-content-center" >
-        <form
-          id="memberLogin"
-         
-          className="card p-4 rounded-4 drop-shadow-20"
-        >
-          <div className="card-body fw-bold px-5 text-IronGray-Deep">
-            <div className="">
-              <label
-                htmlFor="email"
-                className="d-flex justify-content-center m-auto py-3 fs-3"
-              >
-                忘記密碼
-              </label>
-              <input
-                type="email"
-                name="email"
-                className="member-inp border-1 rounded-2"
-                placeholder="請輸入 Email"
-              />
-            </div>
+      {!emailSent ? (
+        <div className="mt-10_5rem d-flex align-items-center justify-content-center">
+          <form id="memberLogin" className="card p-4 rounded-4 drop-shadow-20">
+            <div className="card-body fw-bold px-5 text-IronGray-Deep">
+              <div className="">
+                <label
+                  htmlFor="email"
+                  className="d-flex justify-content-center m-auto py-3 fs-3"
+                >
+                  忘記密碼
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  className="member-inp border-1 rounded-2"
+                  placeholder="請輸入 Email"
+                />
+              </div>
 
-            <div className="d-flex flex-column justify-content-around">
-              <button
-                onClick={handlePass}
-                className="btn btn-login py-2 mb-4 mt-4"
-              >
-                發送驗證信
-              </button>
-              <button onClick={handleClose} className="btn btn-login py-2 mb-4">
-                取消
-              </button>
+              <div className="d-flex flex-column justify-content-around">
+                <button
+                  onClick={handlePass}
+                  className="btn btn-login py-2 mb-4 mt-4"
+                >
+                  發送驗證信
+                </button>
+                <button
+                  onClick={handleClose}
+                  className="btn btn-login py-2 mb-4"
+                >
+                  取消
+                </button>
+              </div>
             </div>
-          </div>
-        </form>
-      </div>
-       ) : null}
+          </form>
+        </div>
+      ) : null}
     </>
   );
 };
