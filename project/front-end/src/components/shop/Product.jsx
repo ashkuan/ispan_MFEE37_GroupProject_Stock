b83103bc6f94d6b01ee16b53b1a0df44ebcc6702
Kristy Yu <tzuchi.yu1@gmail.com>
2023-07-26 01:52:12 +0800

    edit shop.scss

--- project/front-end/src/components/shop/Product.jsx
+++ project/front-end/src/components/shop/Product.jsx
@@ -109,8 +109,8 @@ export const Product = () => {
           <p>
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="40"
-              height="40"
+              width="35"
+              height="35"
               fill="currentColor"
               className="bi bi-filter-left mb-2"
               viewBox="0 0 15 15"
