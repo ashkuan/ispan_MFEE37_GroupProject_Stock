5e539490cc52f015e1f3ea291d330f77002a06e8
Kristy Yu <tzuchi.yu1@gmail.com>
2023-07-25 17:29:22 +0800

    add scss

--- project/back-end/Stock/stock.js
+++ project/back-end/Stock/stock.js
@@ -89,8 +89,51 @@ app.post("/stockName", async function (req, res) {
     console.log(inputValue);
     const stockInfo = StockData.find((item) => item.公司代號 == inputValue);
     const stockName = stockInfo ? stockInfo.公司名稱 : "未找到公司名稱";
+    const stockShortName = stockInfo ? stockInfo.公司簡稱 : "公司簡稱";
+    const stockBuildDate = stockInfo ? stockInfo.成立日期 : "成立日期";
+    const stockDate = stockInfo ? stockInfo.上市日期 : "上市日期";
+    const stockWebsite = stockInfo ? stockInfo.網址 : "網址";
+    const stockAddress = stockInfo ? stockInfo.住址 : "住址";
+    const stockEnglishAddress = stockInfo
+      ? stockInfo.英文通訊地址
+      : "英文通訊地址";
+    const stockPeople1 = stockInfo ? stockInfo.董事長 : "董事長";
+    const stockPeople2 = stockInfo ? stockInfo.總經理 : "總經理";
+    const stockPeople3 = stockInfo ? stockInfo.發言人 : "發言人";
+    const stockPeople4 = stockInfo ? stockInfo.代理發言人 : "代理發言人";
+    const stockPhone = stockInfo ? stockInfo.總機電話 : "總機電話";
+    const stockTax = stockInfo ? stockInfo.傳真機號碼 : "傳真機號碼";
+    const stockEnglishName = stockInfo ? stockInfo.英文簡稱 : "英文簡稱";
+    const stockPrice = stockInfo ? stockInfo.實收資本額 : "實收資本額";
+    const stockEmail = stockInfo ? stockInfo.電子郵件信箱 : "電子郵件信箱";
+    const stockAmount = stockInfo
+      ? stockInfo.已發行普通股數或TDR原股發行股數
+      : "已發行普通股數或TDR原股發行股數";
+    const stockUni = stockInfo
+      ? stockInfo.營利事業統一編號
+      : "營利事業統一編號";
+    const data = {
+      stockName,
+      stockShortName,
+      stockBuildDate,
+      stockDate,
+      stockWebsite,
+      stockAddress,
+      stockEnglishAddress,
+      stockPeople1,
+      stockPeople2,
+      stockPeople3,
+      stockPeople4,
+      stockPhone,
+      stockTax,
+      stockEnglishName,
+      stockPrice,
+      stockEmail,
+      stockAmount,
+      stockUni,
+    };
     console.log(stockName);
-    res.send(stockName);
+    res.send(data);
     // axios
     //   .get("https://openapi.twse.com.tw/v1/exchangeReport/BWIBBU_ALL")
     //   .then((response) => {
